f9ea3e51c749726243b7b7cdca12baf24800d1c5
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-19 17:48:28 +0600

    version 01

---
+++ app/src/main/java/kz/abcsoft/asynctaskdemo6/MainActivity.java
@@ -0,0 +1,81 @@
+package kz.abcsoft.asynctaskdemo6;
+
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    MyTask mt;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("qwe", "create MainActivity: " + this.hashCode());
+
+        tv = (TextView) findViewById(R.id.tv);
+
+        mt = new MyTask();
+        Log.d("qwe", "create MyTask: " + mt.hashCode());
+        mt.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class MyTask extends AsyncTask<String, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                for (int i = 1; i <= 10; i++) {
+                    TimeUnit.SECONDS.sleep(1);
+                    publishProgress(i);
+                    Log.d("qwe", "i = " + i
+                            + ", MyTask: " + this.hashCode()
+                            + ", MainActivity: " + MainActivity.this.hashCode());
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tv.setText("i = " + values[0]);
+        }
+    }
+}
